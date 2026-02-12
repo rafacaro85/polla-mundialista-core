fe03256ba6ff2354bfbd59f96799dba38cb4a976
Rafael <racv85@gmail.com>
2026-02-11 22:46:48 -0500

    Fix CreateSystemSettingDto to include missing fields, resolving validation error on system settings update

--- Polla/apps/api/src/system-settings/dto/create-system-setting.dto.ts
+++ Polla/apps/api/src/system-settings/dto/create-system-setting.dto.ts
@@ -1,1 +1,35 @@
-export class CreateSystemSettingDto {}
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateSystemSettingDto {
+  @IsOptional()
+  @IsString()
+  instagram?: string;
+
+  @IsOptional()
+  @IsString()
+  facebook?: string;
+
+  @IsOptional()
+  @IsString()
+  whatsapp?: string;
+
+  @IsOptional()
+  @IsString()
+  tiktok?: string;
+
+  @IsOptional()
+  @IsString()
+  support?: string;
+
+  @IsOptional()
+  @IsString()
+  copyright?: string;
+
+  @IsOptional()
+  @IsString()
+  termsUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  privacyUrl?: string;
+}
